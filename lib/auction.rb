38b8fce7e24a06cb074a6f80885518b669c7badc
Eli Fuchsman <efuchsman@gmail.com>
2022-09-26 09:19:28 -0600

    Complete it1

--- lib/auction.rb
+++ lib/auction.rb
@@ -6,4 +6,12 @@ class Auction
     @items = []
   end
 
+  def add_item(item)
+  @items << item
+  end
+
+  def item_names
+    @items.map(&:name)
+  end
+
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Auction do
     expect(auction).to be_an_instance_of(Auction)
     expect(auction.items).to eq([])
   end
+
+  it 'can add items and list them by name' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    expect(auction.items).to eq([item1, item2])
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+
+  end
 end
