0c97c8bd249d6790a8646bf1f72b7d3908f7e65c
Eli Fuchsman <efuchsman@gmail.com>
2022-09-26 11:37:01 -0600

    Complete IT4

--- lib/auction.rb
+++ lib/auction.rb
@@ -58,4 +58,20 @@ class Auction
     Date.today.strftime("%d/%m/%Y")
   end
 
+  def close_auction
+    winners_hash = {}
+    @items.each do |item|
+      item.close_bidding
+        if item.bids == {}
+        winners_hash[item] = 'Not Sold'
+        else
+          item.bids.each_pair do |attendee, amount|
+            winners_hash[item] = attendee if amount == item.current_high_bid
+          end
+        end
+      end
+    p winners_hash
+  end
+
+
 end
